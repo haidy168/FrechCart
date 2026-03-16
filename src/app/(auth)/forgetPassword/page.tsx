04d0b3ffbe3c4c471d619da535841c5f30e6d167
haidy yasser <haidy9457@gmail,com>
2026-03-16 12:15:30 +0200

    one

--- src/app/(auth)/forgetPassword/page.tsx
+++ src/app/(auth)/forgetPassword/page.tsx
@@ -1,5 +1,10 @@
-import React from 'react'
+import React from "react";
 
-export default function page() {
-  return <></>
+export default function ForgetPassword() {
+  return (
+    <div>
+      <h1>Forget Password</h1>
+      <p>Please enter your email to reset your password.</p>
+    </div>
+  );
 }
